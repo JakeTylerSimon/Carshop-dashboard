17b214cfe7e9058fe5713dd7f45f8f9d3eb3022b
JakeTylerSimon <jtsimonbaseball@gmail.com>
2022-07-21 16:37:28 -0700

    fina;l

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -24,9 +24,9 @@ const Sidebar = () => {
       {activeMenu && (
         <>
           <div className="flex justify-between items-center">
-            <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
-              <SiShopware /> <span>Shoppy</span>
-            </Link>
+            <div onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+              <SiShopware /> <span><a href="https://carshop-delta.vercel.app/">Grease Garage</a></span>
+            </div>
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
